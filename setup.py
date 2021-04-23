7d1d6d431a606ff56b2fb623e47db5a8a760b58f
James Chapman <james.chapman.19@ucl.ac.uk>
2021-04-23 22:37:34 +0100

    Added more examples

--- setup.py
+++ setup.py
@@ -11,18 +11,16 @@ setup(
     version='0',
     include_package_data=True,
     packages=find_packages(),
-    url='https://github.com/jameschapman19/cca_zoo',
-    license='MIT',
+    url='https://github.com/jameschapman19/pyADMM',
     author='jameschapman',
     description=(
-        'Canonical Correlation Analysis Zoo: CCA, GCCA, MCCA, DCCA, DGCCA, DVCCA, DCCAE, KCCA and regularised variants including sparse CCA , ridge CCA and elastic CCA'
+        'ADMM examples translated to python'
     ),
     long_description=long_description,
     long_description_content_type="text/markdown",
     author_email='james.chapman.19@ucl.ac.uk',
     python_requires='>=3.6',
     install_requires=REQUIRED_PACKAGES,
-    extras_require=DEEP_PACKAGES,
     test_suite='tests',
     tests_require=[],
 )
